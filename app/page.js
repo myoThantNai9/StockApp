0f21a5763446f8fc42b75e218238643897b7c8f5
Myo Thant Naing <myothantnaing@Myos-MacBook-Pro.local>
2025-09-27 18:15:47 +0700

    link

--- app/page.js
+++ app/page.js
@@ -8,8 +8,8 @@ export default function BoxBasic() {
       <Box component="section" className="border border-gray-800 m-5 text-center">
         <h1 className="text-3xl text-violet-950">Stock Management v1.0</h1>
         <ul>
-          <li><a href="/product">Products</a></li>
-          <li><a href="/category">Category</a></li>
+          <li><a href="app/app2/product">Products</a></li>
+          <li><a href="app/app2/category">Category</a></li>
         </ul>
         
       </Box>
